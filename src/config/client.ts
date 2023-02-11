d8ea602fde29095b92586843c6d4102c751e9471
JP Calvo <calvojp92@gmail.com>
2023-02-11 12:40:46 +0800

    x

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,5 +1,18 @@
 import { GraphQLClient } from "graphql-request";
 import { QueryClient } from "react-query";
 
+export const graphqlClient = new GraphQLClient("https://countries.trevorblades.com", {
+  async requestMiddleware(request) {
+    const token = "";
+
+    return {
+      ...request,
+      headers: {
+        ...request.headers,
+        Authorization: `Bearer ${token}`,
+      },
+    };
+  },
+});
+
 export const queryClient = new QueryClient();
-export const graphqlClient = new GraphQLClient("https://countries.trevorblades.com");
