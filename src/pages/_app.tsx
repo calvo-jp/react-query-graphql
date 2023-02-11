5771e514c83cee9c533e7f1f921d584b7789aa98
JP Calvo <calvojp92@gmail.com>
2023-02-11 15:29:59 +0800

    ⚒️

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { queryClient } from "@/config/client";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { Hydrate, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
@@ -9,55 +10,11 @@ export default function App({ Component, pageProps: { dehydratedState, ...pagePr
   const [client] = useState(() => queryClient);
 
   return (
-    <>
+    <Fragment>
       <Head>
         <title>ReactQuery + GraphQL</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-
-      <style jsx global>{`
-        body {
-          font-family: "Oxygen", sans-serif;
-          background-color: white;
-          color: #262626;
-        }
-
-        *,
-        *::after,
-        *::before {
-          margin: 0px;
-          padding: 0px;
-          box-sizing: border-box;
-        }
-
-        ul,
-        ol {
-          list-style-position: inside;
-        }
-
-        table {
-          border-collapse: collapse;
-        }
-
-        td,
-        th {
-          border: 1px solid #d4d4d4;
-          padding: 0.65rem 1rem;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        a:hover {
-          color: #7c3aed;
-        }
-
-        small {
-          font-size: 14px;
-        }
-      `}</style>
 
       <QueryClientProvider client={client}>
         <ReactQueryDevtools position="bottom-right" />
@@ -66,6 +23,6 @@ export default function App({ Component, pageProps: { dehydratedState, ...pagePr
           <Component {...pageProps} />
         </Hydrate>
       </QueryClientProvider>
-    </>
+    </Fragment>
   );
 }
