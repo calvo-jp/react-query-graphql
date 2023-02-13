2b77bb1d1491b3f2ba740c358aa2d6e97b402ebd
JP Calvo <calvojp92@gmail.com>
2023-02-13 18:39:16 +0800

    rm multi proj

--- .graphqlrc.js
+++ .graphqlrc.js
@@ -2,32 +2,28 @@ require("@next/env").loadEnvConfig(process.cwd());
 
 /** @type {import('graphql-config').IGraphQLConfig} */
 module.exports = {
-  projects: {
-    app1: {
-      schema: process.env.NEXT_PUBLIC_COUNTRIES_API,
-      documents: "src/**/*.{gql,graphql}",
-      extensions: {
-        codegen: {
-          generates: {
-            "src/__generated__/graphql.ts": {
-              plugins: [
-                "typescript",
-                "typescript-operations",
-                "typescript-graphql-request",
-                {
-                  add: {
-                    content: "/* eslint-disable */",
-                  },
-                },
-              ],
-              config: {
-                gqlImport: "graphql-request#gql",
-                addDocBlocks: false,
-                dedupeFragments: true,
-                pureMagicComment: true,
-                disableDescriptions: true,
+  schema: process.env.NEXT_PUBLIC_COUNTRIES_API,
+  documents: "src/**/*.{gql,graphql}",
+  extensions: {
+    codegen: {
+      generates: {
+        "src/__generated__/graphql.ts": {
+          plugins: [
+            "typescript",
+            "typescript-operations",
+            "typescript-graphql-request",
+            {
+              add: {
+                content: "/* eslint-disable */",
               },
             },
+          ],
+          config: {
+            gqlImport: "graphql-request#gql",
+            addDocBlocks: false,
+            dedupeFragments: true,
+            pureMagicComment: true,
+            disableDescriptions: true,
           },
         },
       },
